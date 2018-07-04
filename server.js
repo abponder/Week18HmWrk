b9650ea3248f2872821db404ffaffd7b7a3a5e16
alexbponder@gmail.com <alexbponder@gmail.com>
2018-07-03 18:24:50 -0700

    push revised server.js file

--- server.js
+++ server.js
@@ -1,9 +1,64 @@
-   //dependencies
-   const express       = require('express');
-   const bodyparser    = require('body-parser');
-   const exhbs         = require('express-handlebars');
-   const logger        = require('morgan');
-   const mongoose      = require("mongoose");
-
-   const db            = require('./models');
+//dependencies
+const express = require('express');
+const bodyparser = require('body-parser');
+const exhbs = require('express-handlebars');
+const logger = require('morgan');
+const mongoose = require("mongoose");
+const cheerio = require('cheerio')
+const db = require('./models');
 //=====================================================
+
+//initialize the server
+const app = express();
+
+//define port
+const PORT = process.env.PORT || 8080;
+
+//define logger
+app.use(logger('dev'));
+
+//define data parsing
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
+
+//define public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/skyScraperdb";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+});
+
+
+//define routing
+require('./routes')(app);
+
+//================================================================
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+// no stacktraces leaked to user unless in development environment
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: (app.get('env') === 'development') ? err : {}
+    })
+});
+//================================================================
+
+module.exports = app; 
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
